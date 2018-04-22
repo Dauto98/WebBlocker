0dcc7f602eb7bfa0eaf066ae6eb0da85901c038b
Dauto98 <dauto98@gmail.com>
2018-04-22 22:07:52 +0700

    change to matching using regex

--- background.js
+++ background.js
@@ -3,9 +3,11 @@ chrome.webRequest.onBeforeRequest.addListener((detail) => {
 		var data = JSON.parse(localStorage.getItem("data") || '{"block" : [], "pass" : []}');
 
 		for (var i = 0; i < data.block.length; i++) {
-			if (detail.url.includes(data.block[i].url) && data.block[i].state) {
+			var blockRegexUrl = data.block[i].url.replace(/([./])/, "\\$1");
+			if (RegExp(`^(http(?:s))+:\\/\\/(.+?\\.)?${blockRegexUrl}`).test(detail.url) && data.block[i].state) {
 				for (var i = 0; i < data.pass.length; i++) {
-					if (detail.url.includes(data.pass[i].url) && data.pass[i].state) {
+					var passRegexUrl = data.pass[i].url.replace(/([./])/, "\\$1");
+					if (RegExp(`^(http(?:s))+:\\/\\/(.+?\\.)?${passRegexUrl}`).test(detail.url) && data.pass[i].state) {
 						return;
 					}
 				}
